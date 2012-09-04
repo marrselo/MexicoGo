f094e490aa33435857e894be5037ab5e776c9d5f
nazart jara huaman <nazarjb@hotmail.com>
2012-09-04 03:40:37 -0500

    cambios nazart

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,26 +1,59 @@
 <?php
-
-class Default_IndexController extends Zend_Controller_Action
-{
-
-    public function init()
-    {
-        /* Initialize action controller here */
+class Default_IndexController extends Zend_Controller_Action{
+    
+    public function init(){
+        $this->_helper->layout()->setLayout('layout-front');
+        $this->view->headTitle()->setSeparator(' - ');
+        $this->view->headTitle('Mexi-Go!');
+         
     }
 
-    public function indexAction()
-    {
-        // action body
-//        $mail = new CST_Mail();
-//        $mail->setMensaje('hola como estas');
-//        $mail->setAsunto('saludo');
-//        $mail->addDestinatario(array('mail'=>'nazartj@gmail.com','name'=>'nazart jara'));
-//        //$mail->addDestinatario(array('mail'=>'nazartj@gmail.com','name'=>'nazart jara'));
-//        $mail->send();
-//        print_r($mail->getError());
+    public function indexAction(){
         
     }
-
-
+    
+    public function contactAction(){
+        $this->view->headTitle('Contact');
+    }
+    
+    public function magazineAction(){
+        $this->view->headTitle('The Magazine');
+    }
+    
+    public function magazinesubscribeAction(){
+        $this->view->headTitle('The Magazine Subscribe');
+    }
+    
+    public function partnersAction(){
+        $this->view->headTitle('Partners');
+    }
+    
+    public function searchmapAction(){
+        $this->view->headTitle('Search Map');
+    }
+    
+    public function advancesearchAction(){
+        $this->view->headTitle('Advance Search');
+    }
+    
+    public function agentdetailAction(){
+        $this->view->headTitle('Agent Detail');
+    }
+    
+    public function agentsearchAction(){
+        $this->view->headTitle('Agent Search');
+    }
+    
+    public function agentsearch2Action(){
+        $this->view->headTitle('Agent Search');
+    }
+    
+    public function propertydetailAction(){
+        $this->view->headTitle('Property Detail');
+    }
+    
+    public function errorAction(){
+        $this->view->headTitle('Page No Found');
+    }
 }
 
